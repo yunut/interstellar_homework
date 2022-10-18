b4138b61de8c3749e9de5a2a05567c9e25c4ff8c
yunut <somewheres@nate.com>
2022-10-18 16:58:38 +0900

    frontEnd init

--- web/WEB-INF/views/index.jsp
+++ web/WEB-INF/views/index.jsp
@@ -1,9 +1,59 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<script src="/resources/js/index.js"></script>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>인터스텔라 과제테스트</title>
   </head>
   <body>
-    hello word!
+    <table id="amountInfo">
+      <tbody>
+        <tr>
+          <td>
+            지폐금액
+          </td>
+          <td>
+            <input type='text' onkeyup='inputNumberFormat(this);' id='targetMoney'><br/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            동전의 가짓 수
+          </td>
+          <td>
+            <input type='button' id='plus' value='+' onclick="addCoinInfo()"/>
+            <input type='button' id='minus' value='-' onclick="delCoinInfo()"/> <br/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+          </td>
+          <td>
+            <table id = coinInfo>
+              <thead>
+                <tr>
+                  <td>
+                    동전금액
+                  </td>
+                  <td>
+                    동전개수
+                  </td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <input type=text id="coinUnit" onkeyup='inputNumberFormat(this);' style="width:100px" />
+                  </td>
+                  <td>
+                    <input type=text id="coinCount" onkeyup='inputNumberFormat(this);' style="width:100px"/>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          <td/>
+        </tr>
+      </tbody>
+    </table>
+    <input type='button' id="cal" value="계산" onclick="calCoin()"/>
   </body>
 </html>
---
+++ web/resources/js/index.js
@@ -0,0 +1,74 @@
+// 콤마를 붙이는 함수
+function comma(str) {
+    str = String(str);
+    return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
+}
+
+// 콤마를 제거하는 함수
+function uncomma(str) {
+    str = String(str);
+    return str.replace(/[^\d]+/g, '');
+}
+
+function inputNumberFormat(obj) {
+    obj.value = comma(uncomma(obj.value));
+}
+
+
+// + 버튼 클릭 시 가지수 입력 폼 추가
+function addCoinInfo() {
+    var table = document.getElementById("coinInfo");
+
+    var tr = '<tr><td> <input type=text id="coinUnit" onkeyup=\'inputNumberFormat(this);\' value="" style="width:100px"/></td><td><input type=text id="coinCount" value="" style="width:100px"/></td></tr>';
+    table.innerHTML += tr;
+}
+
+// - 버튼 클릭 시 입력 폼 삭제
+function delCoinInfo() {
+    var table = document.getElementById("coinInfo");
+    if(table.rows.length > 2) {
+        table.removeChild(table.childNodes[table.childNodes.length-1]);
+    }
+}
+
+function calCoin() {
+    //XXX 유효성 검사 (input 태그가 비어져있는지, 모두 숫자형식의 데이터가 입력되었는지)
+    var targetMoney = uncomma(document.getElementById("targetMoney").value);
+    var coinInfo = [];
+    var table = document.getElementById("coinInfo");
+    for(var i=1;i<table.rows.length;i++) {
+        var tableTr = table.rows[i];
+        var unit = uncomma(tableTr.cells[0].children[0].value);
+        var count = tableTr.cells[1].children[0].value;
+        var obj = new Object();
+        obj["unit"] = unit;
+        obj["count"] = count;
+        coinInfo.push(obj);
+    }
+
+    // ajax 요청
+    var data = JSON.stringify({
+        targetMoney: targetMoney,
+        coins: coinInfo
+    });
+
+    var xhr = new XMLHttpRequest(); // XMLHttpRequest 객체 생성
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                // 출력 구현
+                console.log("연결 성공");
+            } else {
+                // 실패시  처리 구현
+                console.log("연결 실패");
+            }
+        }
+    }
+    //요청을 보낼 방식, 주소, 비동기 여부 설정
+    xhr.open("POST", "http://localhost:8080/cal.json", true);
+    //요청 해더에 컨텐츠 타입 Json으로 사전 정의
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    //Json형식의 data를 포함하여 요청 전송
+    xhr.send(data);
+}
